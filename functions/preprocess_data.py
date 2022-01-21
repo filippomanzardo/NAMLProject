a4da7e18d804df5216a2b0de245982629932e5d8
Filippo Manzardo <filippommanzardo@gmail.com>
2022-01-21 11:05:55 +0100

    late commit

---
+++ functions/preprocess_data.py
@@ -0,0 +1,143 @@
+import pandas_ta as ta 
+import numpy as np 
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from collections import deque 
+from random import shuffle 
+
+
+def add_indicators(data):
+    
+    data_noLH=data.copy()
+
+    data.ta.log_return(cumulative=True, append=True)
+    data.ta.percent_return(cumulative=True, append=True)
+
+
+    column_LH = ['ISA_9', 'ISB_26', 'ITS_9', 'IKS_26', 'ICS_26', 'DPO_20']
+    base_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+
+    data.ta.strategy(ta.AllStrategy)
+    data.dropna(axis=1, thresh=round(len(data)*0.9), inplace=True)
+    
+    for column in column_LH:
+        try:
+            data.drop(labels=column, inplace=True, axis=1)
+        except:
+            continue
+
+    data_noLH.ta.dpo(lookhead=False)
+    data_noLH.ta.ichimoku(lookhead=False)
+    data_noLH.drop(labels=base_columns, inplace=True, axis=1)
+
+    data = pd.concat([data, data_noLH], axis=1)
+    data.dropna(axis=1, thresh=round(len(data)*0.9), inplace=True)
+    data.dropna(axis=0, inplace=True)
+
+    return data
+
+
+def preprocess_data(data, k=1, column=None, window=None, split=0.8, 
+                    validation_split=0, indicators=True, equalize=True):
+
+    if(indicators):
+        data = add_indicators(data)
+
+    n = len(data)
+    buy = np.zeros(n, dtype=np.int8)
+    for i in range(k,n):
+        if(data.iloc[i-k][column] < data.iloc[i][column]):
+            buy[i-k] = np.int8(1)
+
+    data['Buy'] = buy
+
+    window_data = []
+    labels = []
+    prev_days_features= deque(maxlen=window)
+
+    data = data.to_numpy()
+    n = len(data)
+    train_x = []
+    val_x = []
+    test_x = []
+    num_train = int(split*n)
+    num_validation = int(n*validation_split)
+    
+    train_x = data[0:num_train]
+    val_x = data[num_train:num_train + num_validation] if(validation_split) else np.empty(shape=(0,0))
+    test_x = data[num_train + num_validation:]
+
+    for i in range(train_x.shape[1]-1) :
+        X = train_x[:, i].reshape(-1,1)
+        scaler = StandardScaler().fit(X)
+        train_x[:, i] = scaler.transform(X).reshape(1,-1)
+
+    for i in range(val_x.shape[1]-1):
+        X = val_x[:, i].reshape(-1,1)
+        scaler = StandardScaler().fit(X)
+        val_x[:, i] = scaler.transform(X).reshape(1,-1)
+
+    for i in range(test_x.shape[1]-1):
+        X = test_x[:, i].reshape(-1,1)
+        scaler = StandardScaler().fit(X)
+        test_x[:, i] = scaler.transform(X).reshape(1,-1)
+
+
+    for i in train_x:  # iterate over the values
+        prev_days_features.append([n for n in i[:-1]])  # store all but the target
+        if len(prev_days_features) == window:  
+            window_data.append(np.array(prev_days_features))
+            labels.append(np.int8(i[-1]))
+
+    for i in val_x:  # iterate over the values
+        prev_days_features.append([n for n in i[:-1]])  # store all but the target
+        if len(prev_days_features) == window:  
+            window_data.append(np.array(prev_days_features))
+            labels.append(np.int8(i[-1]))
+
+    for i in test_x:  # iterate over the values
+        prev_days_features.append([n for n in i[:-1]])  # store all but the target
+        if len(prev_days_features) == window:  
+            window_data.append(np.array(prev_days_features))
+            labels.append(np.int8(i[-1]))
+
+    train_x = np.array(window_data[0:num_train])
+    val_x = np.array(window_data[num_train:num_train + num_validation])
+    test_x = np.array(window_data[num_train + num_validation:])
+
+    train_y = np.array(labels[0:num_train]).reshape(-1,1)
+    val_y = np.array(labels[num_train:num_train + num_validation]).reshape(-1,1)
+    test_y = np.array(labels[num_train + num_validation:]).reshape(-1,1)
+
+    if(equalize):
+        n = len(train_x)
+        n_buys = train_y.sum()
+        n_holds = n - n_buys
+
+        if n_buys > n_holds:
+            n_delete = n_buys - n_holds
+            j = 1
+        else:
+            n_delete = n_holds - n_buys
+            j = 0
+
+        shuffled = list(zip(train_x, train_y))
+        shuffle(shuffled)
+
+        (train_x, train_y) = zip(*shuffled)
+
+        train_x, train_y = zip(*shuffled)
+        train_x = np.asarray(train_x)
+        train_y = np.asarray(train_y).reshape(-1,1)
+
+        while(n_delete > 0):
+            for i in range(len(train_x)):
+                if(train_y[i] == j):
+                    train_x = np.delete(train_x, i, axis = 0)
+                    train_y = np.delete(train_y, i, axis = 0)
+                    n_delete -= 1
+                    break
+
+        
+
+    return train_x, val_x, test_x, train_y, val_y, test_y
---
+++ functions/train_test_models.py
@@ -0,0 +1,150 @@
+import tensorflow as tf 
+import numpy as np 
+import sys 
+
+sys.path.insert(0, '../models/')
+
+from sklearn.metrics import confusion_matrix
+from tensorflow import keras 
+from keras import layers
+
+def test_model(model, test_x, test_y):
+    y_hat = model.predict(test_x)
+    for i in range(y_hat.shape[0]):
+        if y_hat[i][0] < 0.5:
+            y_hat[i][0] = 0.
+        else:
+            y_hat[i][0] = 1. 
+
+    buys = test_y.sum()
+    holds = len(test_y)-test_y.sum()
+
+    cm = confusion_matrix(test_y, y_hat)
+    FP = cm.sum(axis=0) - np.diag(cm)  
+    FN = cm.sum(axis=1) - np.diag(cm)
+    TP = np.diag(cm)
+    TN = cm.sum() - (FP + FN + TP)
+
+
+
+    print('Buys: {:d}, Holds: {:d}'.format(buys, holds))
+    print("True Buys: {:d}, False Buys: {:d}, True Holds: {:d}, False Holds: {:d}".format(TP[0],FP[0],TN[0],FN[0]))
+    print("Accuracy: {:.4f}".format(np.count_nonzero((y_hat == np.array(test_y)))/len(test_y)))
+
+def testmodel(input_shape, activation='relu', optimizer=None, loss=None):
+
+    model = keras.Sequential()
+    model.add(layers.Input(shape=input_shape))
+    model.add(layers.Dense(units=256, activation=activation))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(units=1, activation='sigmoid'))
+
+    opt = 'sgd' if optimizer == None else optimizer
+    loss = 'binary_crossentropy' if loss == None else loss
+
+    model.compile(optimizer=opt, loss=loss, metrics=['accuracy'])
+
+    return model
+
+def model0(input_shape, activation='relu', optimizer=None, loss=None):
+
+    model = keras.Sequential()
+    model.add(layers.Input(shape=input_shape, name='input_layer'))
+    model.add(layers.Flatten(name='flatten'))
+    model.add(layers.Dense(units=512, activation=activation, name='dense_1'))
+    model.add(layers.Dense(units=256, activation=activation, name='dense_2'))
+    model.add(layers.Dense(units=128, activation=activation, name='dense_3'))
+    model.add(layers.Dense(units=64, activation=activation, name='dense_4'))
+    model.add(layers.Dense(units=256, activation=activation, name='features_output'))
+    model.add(layers.Dense(units=32, activation=activation, name='dense_5'))
+    model.add(layers.Dense(units=16, activation=activation, name='dense_6'))
+    model.add(layers.Dense(units=1, activation='sigmoid', name='output'))
+
+    opt = 'sgd' if optimizer == None else optimizer
+    loss = 'binary_crossentropy' if loss == None else loss
+
+    model.compile(optimizer=opt, loss=loss, metrics=['accuracy'])
+
+    return model
+
+def model1(input_shape, activation='relu', optimizer=None, loss=None):
+
+    model = keras.Sequential()
+    model.add(layers.Input(shape=input_shape, name='input_layer'))
+
+    model.add(layers.LSTM(units=256, return_sequences=True, name='LSTM_1'))
+    model.add(layers.Dropout(0.4, name='dropout_1'))
+    model.add(layers.BatchNormalization(name='batchnorm_1'))
+
+    model.add(layers.LSTM(units=128, return_sequences=False, name='LSTM_2'))
+    model.add(layers.Dropout(0.2, name='dropout_2'))
+    model.add(layers.BatchNormalization(name='batchnorm_2'))
+
+    model.add(layers.Flatten(name='flatten'))
+    model.add(layers.Dense(units=256, activation=activation, name='features_output'))
+    model.add(layers.Dense(units=64, activation=activation, name='dense_1'))
+    model.add(layers.Dense(units=32, activation=activation, name='dense_2'))
+    model.add(layers.Dense(units=1, activation='sigmoid', name='output'))
+
+    opt = 'sgd' if optimizer == None else optimizer
+    loss = 'binary_crossentropy' if loss == None else loss
+
+    model.compile(optimizer=opt, loss=loss, metrics=['accuracy'])
+
+    return model
+
+def model2(input_shape, activation='relu', optimizer=None, loss=None):
+
+    model = keras.Sequential()
+    model.add(layers.Input(input_shape, name='input_layer'))
+    model.add(layers.Conv1D(filters=64, kernel_size=3, activation=activation, name='convolution1D_1'))
+    model.add(layers.Dropout(0.5, name='dropout_1'))
+    model.add(layers.AveragePooling1D(pool_size=2, name='average_pooling_1'))
+
+    model.add(layers.Conv1D(filters=32, kernel_size=3, activation=activation, name='convolution1D_2'))
+    model.add(layers.Dropout(0.2, name='dropout_2'))
+    model.add(layers.AveragePooling1D(pool_size=2, name='average_pooling_2'))
+
+    model.add(layers.Conv1D(filters=16, kernel_size=1, activation=activation, name='convolution1D_3'))
+    model.add(layers.Dropout(0.2, name='dropout_3'))
+    model.add(layers.GlobalAveragePooling1D(name='global_average'))
+
+    model.add(layers.Dense(units=256, activation=activation, name='features_output'))
+    model.add(layers.Dense(units=32, activation=activation, name='dense_1'))
+    model.add(layers.Dense(32, activation=activation, name='dense_2'))
+    model.add(layers.Dense(1, activation='sigmoid', name='output'))
+
+    opt = 'sgd' if optimizer == None else optimizer
+    loss = 'binary_crossentropy' if loss == None else loss
+
+    model.compile(optimizer=opt, loss=loss, metrics=['accuracy'])
+    
+    return model
+
+def train_model(net, train_data, val_data=None, batch_size=32, 
+                epochs=100, activation=None, optimizer=None, loss=None,
+                verbose=1):
+
+    train_x, train_y = train_data
+    input_shape = train_x.shape[1:]
+
+
+    if net == 0:
+        model = model0(input_shape, activation, optimizer, loss)
+    elif net == 1:
+        model = model1(input_shape, activation, optimizer, loss)
+    elif net == 2:
+        model = model2(input_shape, activation, optimizer, loss)
+    elif net == -1:
+        model = testmodel(input_shape, activation, optimizer, loss)
+    
+
+    model.fit(train_x, 
+            train_y, 
+            verbose=verbose, 
+            batch_size=batch_size, 
+            epochs=epochs,
+            validation_data=val_data,
+    )
+
+    return model
